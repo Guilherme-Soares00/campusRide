0bb78eb36ca37179ce4262666c94d02ca98b1fc3
Gabriel Stuani <bielstuani@gmail.com>
2026-09-16 17:39:52 -0300

    feat(carona): adicionar endpoints de criação e listagem
    Expus as operações iniciais de Carona pela API REST.
    
    A criação retorna 201 Created e a listagem retorna 200 OK.

---
+++ src/main/java/br/com/fiap/campusride/controller/CaronaController.java
@@ -0,0 +1,37 @@
+package br.com.fiap.campusride.controller;
+
+import br.com.fiap.campusride.dto.CaronaRequest;
+import br.com.fiap.campusride.dto.CaronaResponse;
+import br.com.fiap.campusride.service.CaronaService;
+import java.net.URI;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/caronas")
+public class CaronaController {
+
+    private final CaronaService caronaService;
+
+    public CaronaController(CaronaService caronaService) {
+        this.caronaService = caronaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CaronaResponse> criar(@RequestBody CaronaRequest request) {
+        CaronaResponse response = caronaService.criar(request);
+        URI location = URI.create("/caronas/" + response.id());
+
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CaronaResponse>> listar() {
+        return ResponseEntity.ok(caronaService.listar());
+    }
+}
